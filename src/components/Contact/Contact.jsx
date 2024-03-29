38c5717e822ff690595e98641aba84067d2d5984
AnnaP-G <klientkv1511@gmail.com>
2024-03-29 16:49:21 +0200

    hwreact3

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -3,7 +3,7 @@ import css from "./Contact.module.css";
 const Contact = ({ name, number, id, onDeleteContact }) => {
   return (
     <li className={css.contactItem}>
-      <p className={css.contactText}>👤 {name}</p>
+      <p className={css.contactText}>🧝 {name}</p>
       <p className={css.contactText}>📞 {number}</p>
       <button
         className={css.contactBtn}
